ec83de1dfd06c1c3ffbec977d1a1c8246502eb3e
yuyi <523697597@qq.com>
2018-02-14 15:12:09 +0800

    完成课程添加

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,32 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
 
-const login = r => require.ensure([], () => r(require('@/components/Login')), 'login');
-const main = r => require.ensure([], () => r(require('@/components/MainPage')), 'main');
-const mainInfo = r => require.ensure([], () => r(require('@/components/MainInfo')), 'mainInfo');
-const userFeedBack = r => require.ensure([], () => r(require('@/components/UserFeedBack')), 'userFeedBack');
-const addStudent = r => require.ensure([], () => r(require('@/components/addStudent')), 'addStudent');
-const studentList = r => require.ensure([], () => r(require('@/components/studentList')), 'studentList');
-const classList = r => require.ensure([], () => r(require('@/components/classList')), 'classList');
-
+const login = r =>
+  require.ensure([], () => r(require('@/components/Login')), 'login');
+const main = r =>
+  require.ensure([], () => r(require('@/components/MainPage')), 'main');
+const mainInfo = r =>
+  require.ensure([], () => r(require('@/components/MainInfo')), 'mainInfo');
+const userFeedBack = r =>
+  require.ensure(
+    [],
+    () => r(require('@/components/UserFeedBack')),
+    'userFeedBack'
+  );
+const addStudent = r =>
+  require.ensure([], () => r(require('@/components/addStudent')), 'addStudent');
+const studentList = r =>
+  require.ensure(
+    [],
+    () => r(require('@/components/studentList')),
+    'studentList'
+  );
+const classList = r =>
+  require.ensure([], () => r(require('@/components/classList')), 'classList');
+const addClass = r =>
+  require.ensure([], () => r(require('@/components/addClass')), 'addClass');
 
 export default new Router({
   routes: [
@@ -25,30 +41,36 @@ export default new Router({
       component: main,
       children: [
         {
-          path:'',
+          path: '',
           component: mainInfo,
-          meta:['主页信息']
+          meta: ['主页信息']
         },
         {
-          path:'/userFeedBack',
+          path: '/userFeedBack',
           component: userFeedBack,
-          meta:['家长反馈']
+          meta: ['家长反馈']
         },
         {
-          path:'/addStudent',
+          path: '/addStudent',
           component: addStudent,
-          meta:['添加学员']
+          meta: ['添加学员']
         },
         {
-          path:'/studentList',
+          path: '/studentList',
           component: studentList,
-          meta:['学员列表']
-        },{
-          path:'/classList',
-          component:classList,
-          meta:['课程表']
+          meta: ['学员列表']
+        },
+        {
+          path: '/classList',
+          component: classList,
+          meta: ['课程表']
+        },
+        {
+          path: '/addClass',
+          component: addClass,
+          meta: ['添加课程']
         }
       ]
     }
   ]
-})
+});
