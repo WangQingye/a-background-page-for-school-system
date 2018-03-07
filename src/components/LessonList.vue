6c7eb1d4268e2c56511d97b817a8af8982031b4c
yuyi <523697597@qq.com>
2018-03-07 17:24:45 +0800

    改造tong'zh

--- src/components/LessonList.vue
+++ src/components/LessonList.vue
@@ -1,73 +1,97 @@
 <template>
-  <div class="lesson-list">
-    <h1>课程列表</h1>
-    <el-table :data="lesson" style="width: 100%">
-      <el-table-column prop="lessonName" label="课程名称" width="180">
-      </el-table-column>
-      <el-table-column prop="time" label="上课时间" width="180">
-        <template slot-scope="scope">
-          <p v-for="(item,index) of scope.row.time" :key="index">{{item}}</p>
-        </template>
-      </el-table-column>
-      <el-table-column prop="location" label="上课地点">
-        <template slot-scope="scope">
-          <p v-for="(item,index) of scope.row.location" :key="index">{{item}}</p>
-        </template>
-      </el-table-column>
-      <el-table-column prop="teacherName" label="教师名字">
-      </el-table-column>
-      <el-table-column prop="teacherName" label="操作">
-        <template slot-scope="scope">
-          <el-button @click="showClassDetail(scope.$index)" type="text" size="small">
-            查看详情
-          </el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-  </div>
+    <div class="lesson-list">
+        <el-select v-model="school" placeholder="请选择要查看的校区" class="school-select">
+            <el-option v-for="(item,index) in schoolList" :key="index" :label="item.label" :value="item.value">
+            </el-option>
+        </el-select>
+        <h1>课程列表</h1>
+        <el-table :data="lesson" style="width: 100%">
+            <el-table-column prop="lessonName" label="课程名称" width="180">
+            </el-table-column>
+            <el-table-column prop="time" label="上课时间" width="180">
+                <template slot-scope="scope">
+                    <p v-for="(item,index) of scope.row.time" :key="index">{{item}}</p>
+                </template>
+            </el-table-column>
+            <el-table-column prop="location" label="上课地点">
+                <template slot-scope="scope">
+                    <p v-for="(item,index) of scope.row.location" :key="index">{{item}}</p>
+                </template>
+            </el-table-column>
+            <el-table-column prop="teacherName" label="教师名字">
+            </el-table-column>
+            <el-table-column prop="teacherName" label="操作">
+                <template slot-scope="scope">
+                    <el-button @click="showClassDetail(scope.$index)" type="text" size="small">
+                        查看详情
+                    </el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <classDetail @close="closeDetail" :dialogVisible="dialogVisible" :lessonId="lessonId"></classDetail>
+    </div>
 </template>
 <script>
+import ClassDetail from './subpages/ClassDetail';
+import { getSchool } from '../api/getData1';
+
 export default {
+    components: {
+        ClassDetail
+    },
     data() {
         return {
+            dialogVisible: false,
+            lessonId: null,
+            school: null,
+            schoolList: [],
             lesson: [
                 {
-                    lessonId: 123,
+                    lessonId: '123',
                     lessonName: '形体课1班',
                     teacherName: '余老师',
                     time: ['周二/10:00-12:00'],
                     location: ['C教室']
-                },
-                {
-                    lessonId: 123,
-                    lessonName: '形体课2班',
-                    teacherName: '刘老师',
-                    time: ['周一/17:30-19:00', '周二/10:00-12:00'],
-                    location: ['F教室', 'D教室']
                 }
             ]
         };
     },
+    created() {
+        this.getSchoolList();
+    },
+    watch: {
+        school(val) {
+            // this.getClass();
+            console.log(val);
+        }
+    },
     methods: {
-        objectSpanMethod({ row, column, rowIndex, columnIndex }) {
-            if (columnIndex === 0) {
-                if (rowIndex % 2 === 0) {
-                    return {
-                        rowspan: 2,
-                        colspan: 1
-                    };
-                } else {
-                    return {
-                        rowspan: 0,
-                        colspan: 0
-                    };
-                }
-            }
+        showClassDetail(index) {
+            this.lessonId = this.lesson[index].lessonId;
+            this.dialogVisible = true;
+        },
+        closeDetail() {
+            this.dialogVisible = false;
+        },
+        async getSchoolList() {
+            // 获取校区
+            const res = await getSchool();
+            res.list.forEach(element => {
+                var temp = {
+                    value: element.name,
+                    label: element.name
+                };
+                this.schoolList.push(temp);
+            });
+            this.school = this.schoolList[0].value;
         }
     }
 };
 </script>
 <style lang="less">
-
+.school-select {
+    width: 900px;
+    margin: 20px auto;
+}
 </style>
 
--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -3,37 +3,32 @@
         <h1>发送通知</h1>
         <el-form ref="form" :model="form" label-width="80px">
             <el-form-item label="通知内容">
-                <el-input type="textarea" :rows="5" v-model="form.desc"></el-input>
+                <el-select v-model="form.content" placeholder="请选择要发送的通知" class="content">
+                    <el-option v-for="(item,index) in contentList" :key="index" :label="item.label" :value="item.value">
+                    </el-option>
+                </el-select>
             </el-form-item>
 
             <el-form-item label="发送时间">
-                <el-col :span="11">
-                    <el-date-picker value-format="yyyy-MM-dd" type="date" placeholder="选择日期" v-model="form.day" style="width: 100%;"></el-date-picker>
-                </el-col>
-                <el-col class="line" :span="2">-</el-col>
-                <el-col :span="11">
-                    <el-time-picker value-format="hh:mm:ss" type="fixed-time" placeholder="选择时间" v-model="form.time" style="width: 100%;"></el-time-picker>
-                </el-col>
+                <el-date-picker value-format="yyyy-MM-dd" type="date" placeholder="选择日期" v-model="form.day" class="day"></el-date-picker>
+                <el-time-select v-model="form.time" :picker-options="{start: '08:30',step: '00:15',end: '24:00'}" placeholder="选择时间" class="time">
+                </el-time-select>
             </el-form-item>
-            <el-form-item label="全体发送">
-                <el-switch v-model="form.allStudent"></el-switch>
+            <el-form-item label="发送方式">
+                <el-select v-model="form.type" placeholder="请选择要发送方式" class="type">
+                    <el-option v-for="(item,index) in typeList" :key="index" :label="item.label" :value="item.value">
+                    </el-option>
+                </el-select>
             </el-form-item>
-            <el-form-item label="按班级">
-                <el-transfer :props="{
-      key: 'classId',
-      label: 'className'
-    }" v-model="form.class" :data="classList" :titles="['班级列表','通知的班级']"></el-transfer>
+            <el-form-item label="按班级" v-show="form.type == '2'">
+                <el-transfer  :props="{key: 'classId',label: 'className'}" v-model="form.class" :data="classList" :titles="['班级列表','通知的班级']" class="select-item"></el-transfer>
             </el-form-item>
-            <el-form-item label="按学生">
-                <el-transfer :props="{
-      key: 'studentId',
-      label: 'studentName'
-    }" v-model="form.student" :data="studentList" :titles="['学生列表','通知的学生']"></el-transfer>
+            <el-form-item label="按学生" v-show="form.type =='3'">
+                <el-transfer  :props="{key: 'studentId',label: 'studentName'}" v-model="form.student" :data="studentList" :titles="['学生列表','通知的学生']" class="select-item"></el-transfer>
             </el-form-item>
-
             <el-form-item>
-                <el-button type="primary" @click="addNotification">添加通知</el-button>
-                <el-button>取消</el-button>
+                <el-button class="add-btn" type="primary" @click="addNotification">添加通知</el-button>
+                <el-button class="add-btn" type="info">取消</el-button>
             </el-form-item>
         </el-form>
     </div>
@@ -42,15 +37,43 @@
 export default {
     data() {
         return {
+            contentList: [
+                {
+                    value: '1',
+                    label: '尊敬的家长。。。。。。。'
+                },
+                {
+                    value: '2',
+                    label: '尊敬的家长。。。。。。。'
+                },
+                {
+                    value: '3',
+                    label: '尊敬的家长。。。。。。。'
+                }
+            ],
+            typeList: [
+                {
+                    label: '全体发送',
+                    value: 1
+                },
+                {
+                    label: '按班级发送',
+                    value: 2
+                },
+                {
+                    label: '按学生发送',
+                    value: 3
+                }
+            ],
             form: {
                 // 通知内容
-                desc: null,
+                content: null,
                 // 发送日期
                 day: null,
                 // 具体时间
                 time: null,
                 // 是否全体发送通知
-                allStudent: false,
+                type: 1,
                 // 要通知的班级Id
                 class: [],
                 // 要通知的学生Id
@@ -119,6 +142,27 @@ export default {
     margin: 0 auto;
     .el-transfer-panel__item .el-checkbox__input {
         left: 20px;
+    }
+    .content {
+        width: 615px;
+    }
+    .day {
+        width: 392px;
+        margin-right: 20px;
+    }
+    .time {
+        width: 200px;
+    }
+    .type {
+        width: 615px;
+    }
+    .select-item{
+        margin-bottom:20px;
+        margin-left:20xp;
+    }
+    .add-btn{
+        width:200px;
+        margin-right:20px;
     }
 }
 </style>
